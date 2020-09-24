12e6428aed72c0ad6f039e009779fb1eeb0c4b63
sean rice <contact@sean.ee>
2020-09-24 00:11:33 -0400

    first commit (dotnet new mvc & identity scaffold)

---
+++ Cs6065_Homework1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Cs6065_Homework1.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Cs6065_Homework1.Areas.Identity.IdentityHostingStartup))]
+namespace Cs6065_Homework1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
